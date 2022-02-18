92780b14a028df8b928a0b276222c4cc9ce5d7cd
harshithpabbati <pabbatiharshith@gmail.com>
2022-02-18 21:25:31 +0530

    initial commit

---
+++ components/useBreakoutRoom.ts
@@ -0,0 +1,50 @@
+import { DailyCall } from "@daily-co/daily-js";
+
+const getDateTimeAfter = (minutes: number) => {
+  const date = new Date(new Date().getTime() + minutes * 60000);
+  return Math.round(date.getTime() / 1000);
+};
+
+const useBreakoutRoom = (call: DailyCall) => {
+  const createSession = async (config: any) => {
+    const rooms: Array<any> = [];
+    const p = await call.participants();
+    const participants = Object.values(p);
+
+    Array.from({length: Math.ceil(participants.length / config.rooms)}, (val, i) => {
+      rooms.push({
+        name: `Breakout room ${i + 1}`,
+        room_url: `forj-breakout-${i + 1}`,
+        created: new Date(),
+        participants: participants.slice(i * config.rooms, i * config.rooms + config.rooms).map(p => p.user_id),
+      });
+    });
+
+    const sessionObject = {
+      rooms,
+      config: {
+        auto_join: config.auto_join,
+        allow_user_exit: config.allow_user_exit,
+        exp: config.exp ? getDateTimeAfter(config.expiryTime): null
+      }
+    }
+
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        sessionObject,
+        event: 'DAILY_BREAKOUT_STARTED'
+      }),
+    };
+
+    const res = await fetch('/api/pusher', options);
+    const { status } = await res.json();
+    return status;
+  };
+
+  return {
+    createSession,
+  }
+};
+
+export default useBreakoutRoom;
