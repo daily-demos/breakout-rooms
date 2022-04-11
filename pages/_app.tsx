4becf18ed4977c8a28247b3e354409d704bd17e6
harshithpabbati <pabbatiharshith@gmail.com>
2022-04-11 15:49:09 +0530

    Update primary theme color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,30 @@ import '../styles/Home.css';
 import type { AppProps } from 'next/app';
 import { CallProvider } from '../contexts/CallProvider';
 import { BreakoutRoomProvider } from '../contexts/BreakoutRoomProvider';
+import { ThemeProvider, mergeTheme, defaultTheme } from 'evergreen-ui';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const theme = mergeTheme(defaultTheme, {
+    components: {
+      Button: {
+        appearances: {
+          primary: {
+            backgroundColor: '#1BEBB9',
+            color: 'black',
+          }
+        },
+      },
+    },
+  });
+
   return (
-    <CallProvider>
-      <BreakoutRoomProvider>
-        <Component {...pageProps} />
-      </BreakoutRoomProvider>
-    </CallProvider>
+    <ThemeProvider value={theme}>
+      <CallProvider>
+        <BreakoutRoomProvider>
+          <Component {...pageProps} />
+        </BreakoutRoomProvider>
+      </CallProvider>
+    </ThemeProvider>
   );
 }
 
