84691788a0875b309efbe1192a959c84dff8a8e9
harshithpabbati <pabbatiharshith@gmail.com>
2022-05-23 14:19:53 +0530

    fix middleware issue

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -1,12 +1,12 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
-const isLocalHost = (url: string) => {
-  return url.indexOf('localhost') !== -1 || url.indexOf('127.0.0.1') !== -1;
-};
+type Environment = 'production' | 'development';
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const currentEnv = process.env.ENV as Environment;
+
   if (
-    !isLocalHost(req.url) &&
+    currentEnv === 'production' &&
     req.headers.get('x-forwarded-proto') !== 'https'
   ) {
     return NextResponse.redirect(
