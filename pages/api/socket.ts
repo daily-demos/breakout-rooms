f359a55d3c05c0d59a2287cb6963320aa0e98ec0
harshithpabbati <pabbatiharshith@gmail.com>
2022-08-21 22:44:31 +0530

    Check if room exists before we create room, if it exists just update the room with the properties

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -11,6 +11,7 @@ export default async function handler(
 
   if (req.method === 'POST') {
     if (event === 'DAILY_BREAKOUT_STARTED') {
+      let retCode;
       // create daily breakout rooms.
       sessionObject.rooms.map(async (room: DailyBreakoutRoom) => {
         // Basic, required room properties
@@ -23,13 +24,20 @@ export default async function handler(
           exp: sessionObject.config.exp,
         };
 
-        let retCode = await createRoom(room.roomName, roomProperties, {
-          enable_recording: 'cloud',
-        });
+        const alreadyExists = await checkIfRoomExists(room.roomName);
+        if (alreadyExists) {
+          retCode = await updateRoomProperties(room.roomName, roomProperties, {
+            enable_recording: 'cloud',
+          });
+        } else {
+          retCode = await createRoom(room.roomName, roomProperties, {
+            enable_recording: 'cloud',
+          });
 
-        // If something went wrong, early out with response code
-        if (retCode !== 200) {
-          return res.status(retCode);
+          // If something went wrong, early out with response code
+          if (retCode !== 200) {
+            return res.status(retCode);
+          }
         }
       });
     }
@@ -41,6 +49,77 @@ export default async function handler(
     return res.status(200).json({ status: 'success' });
   }
   return res.status(500);
+}
+
+// trying to check if the room already exists.
+function checkIfRoomExists(roomName: string) {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.DAILY_API_KEY}`,
+    },
+  };
+
+  const url: string = `${
+    process.env.DAILY_API_URL || 'https://api.daily.co/v1'
+  }/rooms/${roomName}`;
+  return fetch(url, options).then(async response => {
+    return response.status === 200;
+  });
+}
+
+function updateRoomProperties(
+  roomName: string,
+  properties: { [key: string]: string | boolean | number },
+  additionalProperties: {
+    [key: string]: string | boolean | number;
+  } | null = null,
+): Promise<number> {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.DAILY_API_KEY}`,
+    },
+    body: JSON.stringify({
+      privacy: 'private',
+      // Basic + additional properties
+      properties: {
+        ...properties,
+        ...additionalProperties,
+      },
+    }),
+  };
+
+  const url: string = `${
+    process.env.DAILY_API_URL || 'https://api.daily.co/v1'
+  }/rooms/${roomName}`;
+  return fetch(url, options).then(async response => {
+    // If everything is OK, just return 200
+    if (response.status === 200) return 200;
+    // If something went wrong, get response body
+    const data = await response.json();
+    // Check if one of the additional properties is actually the problematic one,
+    // and retry without it if so
+    const errorMsg = data?.info;
+    console.log(
+      `Failed to update breakout room. Status code: ${response.status}, error: ${data?.info}`,
+    );
+    if (response.status === 400 && additionalProperties && errorMsg) {
+      for (const property in additionalProperties) {
+        if (errorMsg.includes(`'${property}'`)) {
+          delete additionalProperties[property];
+          return updateRoomProperties(
+            roomName,
+            properties,
+            additionalProperties,
+          );
+        }
+      }
+    }
+    return 500;
+  });
 }
 
 // createRoom attempts to createa a Daily room.
